4f89719196c81826b0e30bccea6f141470605f58
David Nelson <david.nelson1@uqconnect.edu.au>
2025-03-14 16:10:12 +1000

    readAll, read, update and delete in alfaLessonDbInterface

--- alfaLesson/alfaLessonDbInterface.ts
+++ alfaLesson/alfaLessonDbInterface.ts
@@ -20,4 +20,38 @@ export default class alfaLessonDbInterface {
     alfaLessonDbInterface.alfaLessons.push(newAlfaLesson);
     return newAlfaLesson;
   }
+
+  static readAll = (): AlfaLesson[] => {
+    return alfaLessonDbInterface.alfaLessons;
+  };
+
+  static read = (lessonId: number): AlfaLesson | undefined => {
+    return alfaLessonDbInterface.alfaLessons.find(
+      (lesson) => lesson.id === lessonId
+    );
+  };
+  
+  static update = (
+    lessonId: number,
+    updateData: Partial<Omit<AlfaLesson, 'id' | 'createdAt' | 'updatedAt'>>
+  ): AlfaLesson | undefined => {
+    const alfaLesson = alfaLessonDbInterface.alfaLessons.find(
+      (alfaLesson) => alfaLesson.id === lessonId
+    );
+    if (alfaLesson) {
+      Object.assign(alfaLesson, updateData, { updatedAt: new Date() });
+    };
+    return alfaLesson;
+  };
+
+  static delete = (lessonId: number): boolean => {
+    const index = alfaLessonDbInterface.alfaLessons.findIndex(
+      (lesson) => lesson.id === lessonId
+    );
+    if (index !== -1) {
+      alfaLessonDbInterface.alfaLessons.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
 }
