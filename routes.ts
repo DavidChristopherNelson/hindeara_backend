52e040f783f9c90e635a562d91e199241241bbeb
David Nelson <david.nelson1@uqconnect.edu.au>
2025-03-14 15:40:05 +1000

    Remove chat-gpt lesson code.

--- routes.ts
+++ routes.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // AlfaLesson routes
-app.post('/', (req, res) => {
+app.post('/alfa_lesson', (req, res) => {
   res.send(alfaLessonController.create(req, res));
 })
 
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => {
   res.send('OK');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
