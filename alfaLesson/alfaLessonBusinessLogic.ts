547fb76d29cd7f8191e95a127070b55af86e3093
David Nelson <david.nelson1@uqconnect.edu.au>
2025-03-14 16:16:19 +1000

    readAll, read, update and delete in alfaLessonBusinessLogic

--- alfaLesson/alfaLessonBusinessLogic.ts
+++ alfaLesson/alfaLessonBusinessLogic.ts
@@ -13,4 +13,23 @@ export default class alfaLessonBusinessLogic {
     );
     return newAlfaLesson;
   }
+
+  static readAll = (): AlfaLesson[] => {
+    return alfaLessonDbInterface.readAll();
+  }
+
+  static read = (alfaLessonId: number): AlfaLesson | undefined => {
+    return alfaLessonDbInterface.read(alfaLessonId);
+  }
+
+  static update = (
+    alfaLessonId: number,
+    updateData: Partial<Omit<AlfaLesson, 'id' | 'createdAt' | 'updatedAt'>>
+  ): AlfaLesson | undefined => {
+    return alfaLessonDbInterface.update(alfaLessonId, updateData);
+  }
+
+  static delete = (alfaLessonId: number): boolean => {
+    return alfaLessonDbInterface.delete(alfaLessonId);
+  }
 }
