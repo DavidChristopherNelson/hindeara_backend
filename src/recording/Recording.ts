2e84f4dc521a1770ec1bd181ab3177e53e57f93b
David Nelson <david.nelson1@uqconnect.edu.au>
2025-03-25 10:40:24 +1000

    Create Recording Class

---
+++ src/recording/Recording.ts
@@ -0,0 +1,23 @@
+export interface Recording {
+  id: number;
+  studentId: number;
+  recording: Buffer;
+  createdAt: Date;
+  updatedAt: Date;
+  alfaLessonId?: string;
+  transcription?: string;
+  evaluation?: string;
+}
+
+export class RecordingClass implements Recording {
+  constructor(
+    public id: number,
+    public studentId: number,
+    public recording: Buffer,
+    public createdAt: Date = new Date(),
+    public updatedAt: Date = new Date(),
+    public alfaLessonId?: string,
+    public transcription?: string,
+    public evaluation?: string
+  ) {}
+}
